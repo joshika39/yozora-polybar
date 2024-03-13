d794ac8baa6ab05360bcca65c6b800d111ce5281
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-13 21:28:27 +0100

    removed old script

--- scripts/battery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Get the battery percentage or return 0 if the battery is not present
-battery=$(sudo tlp-stat -b | tac | grep -m 1 "Charge" |  tr -d -c "[:digit:],.")
-if [ -z "$battery" ]; then
-    battery=0
-fi
-
-echo "# $battery %"
