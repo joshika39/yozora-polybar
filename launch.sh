621dae37e9e179c8ff8d55dac61feb8d615b98b5
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-13 10:08:10 +0100

    fixed lanuch sh

--- launch.sh
+++ launch.sh
@@ -13,6 +13,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar -c ~/.config/polybar/config.ini --reload main &
-# kitty polybar -l info -c ~/.config/polybar/config.ini --reload main-top & 
-polybar -c ~/.config/polybar/config.ini --reload main-top &
 polybar -c ~/.config/polybar/config.ini --reload secondary &
