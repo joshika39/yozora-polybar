60f1b947e6acf6ef88faa9bdacb3a92bc4556d91
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-12-03 15:51:59 +0100

    commented out restart

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@
 export POLYBAR_HOME=~/.config/polybar
 
 # Terminate already running bar instances
-killall -q polybar
+# killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
